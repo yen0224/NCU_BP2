0e3d23ebbca15d2700fa014a9438cd274e3fe26c
yen <yen0224@pm.me>
2021-12-16 10:20:03 +0800

    uploaded hw1.c

---
+++ wk6/hw1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int sq[10], ct[10];
+    int count = 10;
+    int input;
+    int check = 0;
+    //初始化
+    for (int i = 0; i < 10; i++)
+    {
+        ct[i] = 0;
+    }
+
+    //輸入
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &input);
+        sq[i] = input;
+        ct[input]++;
+
+        /*if(getchar()=='\n'){
+            count=i;
+            break;
+        }*/
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        if (!i)
+        {
+            printf("%d\t%d\n", sq[i], ct[sq[i]]);
+        }
+        else
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (sq[j] == sq[i])
+                {
+                    check = 0;
+                    break;
+                }
+                else
+                {
+                    check = 1;
+                }
+            }
+            if (check)
+                printf("%d\t%d\n", sq[i], ct[sq[i]]);
+        }
+    }
+
+    return 0;
+}
