0f105ddb8e35e6bbc4966418679633fa39653e06
yen <yen0224@pm.me>
2021-12-23 11:01:51 +0800

    upload hw4.c

---
+++ wk6/hw4.c
@@ -0,0 +1,41 @@
+//去除重複
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int Sample[20];
+    int input;
+    //輸入陣列
+    for (int i = 0; i < 20; i++){
+        scanf("%d", &input);
+        Sample[i]=input;
+    }
+    // bubble sort
+    for (int i = 0; i < 20; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            if (Sample[j] > Sample[i])
+            {
+                int temp = Sample[j];
+                Sample[j] = Sample[i];
+                Sample[i] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 20; i++)
+    {
+        if(i!=0){
+            if(Sample[i]==Sample[i-1]){
+                continue;
+            }
+            else{
+                printf(" %d",Sample[i]);
+            }
+        }
+        else{
+            printf("%d",Sample[i]);
+        }
+    }
+    
+    return 0;
+}
