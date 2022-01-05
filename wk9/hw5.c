6145cfcd33e4451933470991bcfd18e2082eae3d
yen0224 <yen0224@pm.me>
2022-01-06 00:58:19 +0800

    bug fix for 9-5

--- wk9/hw5.c
+++ wk9/hw5.c
@@ -2,10 +2,10 @@
 #include <string.h>
 int main(int argc, char const *argv[])
 {
-    char str[10];
+    char str[11];
     printf("input>");
-    fgets(str,10,stdin);
-    str[strlen(str)-1]='\0';
+    fgets(str,11,stdin);
+    if (str[strlen(str)-1]=='\n')str[strlen(str)-1]='\0';
     printf("--output--\n");
     printf("\"%s\"\n",str);
     printf("\\%-20s\\\n",str);
