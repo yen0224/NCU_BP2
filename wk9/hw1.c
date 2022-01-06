e3a757e42719cc2b3803bb8f1d378068ccd5d815
yen <yen0224@pm.me>
2022-01-06 12:58:21 +0800

    delete symbols

--- wk9/hw1.c
+++ wk9/hw1.c
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
     float input;
     printf("please input the temperature (Fahrenheit)");
     scanf("%f",&input);
-    printf("%+10.3f ˚F\n",input);
-    printf("%+10.3f ˚C\n",(input-32)*5/9);
+    printf("%+10.3f F\n",input);
+    printf("%+10.3f C\n",(input-32)*5/9);
     return 0;
 }
