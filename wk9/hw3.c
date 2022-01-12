7af999a7409d3abd05c34d2813485a54f2915947
yen <yen0224@pm.me>
2022-01-12 19:58:11 +0800

    edit hw9-3

--- wk9/hw3.c
+++ wk9/hw3.c
@@ -3,12 +3,7 @@ int main(int argc, char const *argv[])
 {
     int cellnum,row;
     scanf("%d",&cellnum);
-    if(cellnum<702){
-        row=1;
-    }
-    else{
-        row=cellnum/702+1;
-    }
+    row=cellnum/702+1;
     int col=cellnum%702;
     printf("%d-",row);
     if(col<=26)printf("%c",col+64);
